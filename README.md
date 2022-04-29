99c48f948aa3f2e6f888402fc309af953de6883c
Alexander Gräb <mail@hpvg.de>
2022-04-29 20:23:37 +0200

    Initial commit
    Added a README file and very basic code.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kolkturm Viewer 360
+
+Der [Kolkturm](https://de.wikipedia.org/wiki/Kolkturm) ist ein Aussichtspunkt in Halle an der Saale. Der Kolkturm Viewer 360 erlaubt einen virtuellen Rundumblick. Objekte in der Ferne werden erklärt, wenn man mit dem Mauspfeil darüber fährt.
---
+++ kolkviewer/main.js
@@ -0,0 +1,19 @@
+function draw() {
+    var canvas = document.getElementById('ktv');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        ctx.font = '48px serif';
+        ctx.fillText('Lade...', 10, 50);
+
+        var img = new Image();
+        img.onload = function() {
+            cw = canvas.clientWidth;
+            iw = img.width;
+            s = cw / iw;
+            //ctx.drawImage(img, 0, 0);
+            ctx.drawImage(img, 0, 0, 1800, s*img.height);
+        }
+        img.src = 'img/kolkturm360.png';
+    }
+}
