48393373a1f0945915fe2fc11d58730406f37a02
Alexander Gräb <mail@hpvg.de>
2022-05-03 19:09:28 +0200

    Introduce init function

--- kolkviewer/main.js
+++ kolkviewer/main.js
@@ -1,26 +1,42 @@
-function draw() {
-    var canvas = document.getElementById('ktv');
+var canvas = null;
+var wrapper = null;
+var pano = null;
+var vw = null;
+var vh = null;
+var vr = null; // View ratio
+
+function initKolkViewer(wrapper_id, canvas_id, img_url) {
+    wrapper = $("#" + wrapper_id).get(0);
+    canvas = $("#" + canvas_id).get(0);
+
+    vw = $(wrapper).width();
+    vh = $(wrapper).height();
+    vr = vw/vh;
+
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
+
+        canvas.width = vw;
+        canvas.height = vh;
 
         ctx.font = '48px serif';
         ctx.fillText('Lade...', 10, 50);
 
-        var img = new Image();
-        img.onload = function() {
-            vw = $( wktv ).width();
-            vh = $( wktv ).height();
-            vr = vw/vh; // View ratio
-
-            canvas.width = vw;
-            canvas.height = vh;
-
-            cw = canvas.clientWidth;
-            iw = img.width;
-            ih = img.height;
-            s = vh / ih;
-            ctx.drawImage(img, 0, 0, vr*ih, ih, 0, 0, vw, vh);
+        pano = new Image();
+        pano.onload = function() {
+            draw();
         }
-        img.src = 'img/kolkturm360.png';
+        pano.src = img_url;
+    }
+}
+
+function draw() {
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+        var iw = pano.width;
+        var ih = pano.height;
+        var s = vh / ih;
+
+        ctx.drawImage(pano, 0, 0, vr*ih, ih, 0, 0, vw, vh);
     }
 }
